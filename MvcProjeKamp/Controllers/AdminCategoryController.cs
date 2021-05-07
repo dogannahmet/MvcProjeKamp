4c52c1e54b4be1a8819938f6041d25c6fab3bc07
Ahmet D <ahmet.msn@live.com>
2021-05-07 13:54:36 +0300

    AddedTheme<07.05>

---
+++ MvcProjeKamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                categoryManager.Add(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
